263bb4021a9f482cb574e5f61a560fa7b1430219
Meera M Nair <meeramnair2022@cs.ajce.in>
2021-04-09 21:05:07 +0530

    Background Image

--- login.js
+++ login.js
@@ -1,30 +1,3 @@
-// $("document").ready(function(){
-//     var id = $("#userid").val();
-//         var pwd =$("#exampleInputPassword1").val();
-//         console.log(id);
-//         console.log(pwd);
-//     $("#submitme").click(function(){
-//         if(pwd==''&&id==''){
-//             alert("Fields cannot be empty");
-//         }
-//         else if(id==''){
-//             alert("UserId cannot be empty");
-//         }
-//         else if(pwd==''){
-//             alert("Passwordcannot be empty");
-//         }
-//         else if(id == 'admin'&& pwd == '12345'){
-//             redirecting();
-//         }
-//         else{
-//             alert("INVALID CREDENTIALS!!");
-//         }
-//     });
-// });
-// function redirecting(){
-//     $("#newform").attr("action","index.html");
-// }
-
 var id = document.getElementById("userid");
 var pwd= document.getElementById("exampleInputPassword1");
 function check(redirecting){
@@ -52,4 +25,32 @@ function redirecting(){
     var form = document.getElementById("newform");
     form.setAttribute("action","sp.html");
 }
+
+// $("document").ready(function(){
+//     var id = $("#userid").val();
+//         var pwd =$("#exampleInputPassword1").val();
+//         console.log(id);
+//         console.log(pwd);
+//     $("#submitme").click(function(){
+//         if(pwd==''&&id==''){
+//             alert("Fields cannot be empty");
+//         }
+//         else if(id==''){
+//             alert("UserId cannot be empty");
+//         }
+//         else if(pwd==''){
+//             alert("Passwordcannot be empty");
+//         }
+//         else if(id == 'admin'&& pwd == '12345'){
+//             redirecting();
+//         }
+//         else{
+//             alert("INVALID CREDENTIALS!!");
+//         }
+//     });
+// });
+// function redirecting(){
+//     $("#newform").attr("action","index.html");
+// }
+
 
